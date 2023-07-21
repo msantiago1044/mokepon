252a4d205a8bb9321b0aadf1f8342c24dce97188
Marcelo Barraza <97839381+msantiago1044@users.noreply.github.com>
2023-07-20 22:55:05 -0500

    Final PC 20072023
    terminado hasta la clase 60 sin agregar mas personajes

--- Mokepon/js/codigo.js
+++ Mokepon/js/codigo.js
@@ -1,16 +1,17 @@
-let ataqueJugador = ""
-let ataqueDelRival = ""
+let ataquesDelJugador = []
+let ataquesDelRival = []
 let triunfos = 0
 let derrotas = 0
 let empates = 0
-let vidaTuya = 3
-let vidaMaquina = 3
+let vidaJugador = 3
+let vidaRival = 3
 let mokepones = []
+let resultado = ''
 let opcionDeMokepones
 let opcionDeAtaques
 
 let ocultarSecciones = document.getElementById("seccion-batalla")
-let botonMascota = document.getElementById("mascota")
+let botonSeleccionMascota = document.getElementById("mascota")
 let botonReinicio = document.getElementById("reinicio")
 let mascotaSeleccionadaJudador = document.getElementById("mascota-seleccionada")
 let mascotaSeleccionadaRival = document.getElementById("mascota-rival")
@@ -26,9 +27,13 @@ let botonViento = ""
 let inputHidropone = ""
 let inputRatagueya = ""
 let inputCimiodonte = "" 
-let mascotaJugador = "" 
+let mascotaJugador = ""
+let mascotaRival = ""
 let botones = []
-let ataquesJugador
+let ataquesMokeponEnemigo = []
+let ataquesMokeponJugador = []
+let indexAtaqueEnemigo = ""
+let indexAtaqueJugador = ""
 
 class Mokepones {
     constructor(nombre, foto, vida){
@@ -79,7 +84,7 @@ function iniciarJuego() {
         inputRatagueya = document.getElementById("Ratagueya")
         inputCimiodonte = document.getElementById("Cimiodonte") 
     })    
-    botonMascota.addEventListener("click", seleccionMascotaJugador)
+    botonSeleccionMascota.addEventListener("click", seleccionMascotaJugador)
     botonReinicio.addEventListener("click", reinicio)
 }
 function seleccionMascotaJugador() { 
@@ -103,20 +108,21 @@ function ranNumero(min, max) {
     return Math.floor(Math.random()*(max-min+1)+min)
 } 
 function seleccionMascotaRival() {
-    pc = ranNumero(0,mokepones.length-1)
-    mascotaSeleccionadaRival.innerHTML = mokepones[pc].nombre
+    mascotaRival = ranNumero(0,mokepones.length-1)
+    mascotaSeleccionadaRival.innerHTML = mokepones[mascotaRival].nombre
+    ataquesMokeponEnemigo = mokepones[mascotaRival].ataques
     ocultarSecciones.style.display = "flex"
     ocultarSeccionesI.style.display = "none"
+    secuenciaAtaques()
 }
-function extraerAtaques(mascotaJugador) {
-    let ataques = []
+function extraerAtaques(mascota) {
+    let ataquesMokeponJugador = []
     for (let i = 0; i < mokepones.length; i++) {
-        if (mascotaJugador == mokepones[i].nombre) {
-            ataques = mokepones[i].ataques;
-        }
-        
+        if (mascota == mokepones[i].nombre) {
+            ataquesMokeponJugador = mokepones[i].ataques;
+        }        
     }
-    mostrarAtaques(ataques)
+    mostrarAtaques(ataquesMokeponJugador)
 }
 function mostrarAtaques(ataques) {
     ataques.forEach((ataque) => {
@@ -126,81 +132,97 @@ function mostrarAtaques(ataques) {
         botonAgua = document.getElementById("ataque-agua")
         botonViento = document.getElementById("ataque-viento")
     })  
-    botones = document.querySelectorAll(".BAtaque")    
-    botonFuego.addEventListener("click", ataqueFuego)    
-    botonAgua.addEventListener("click", ataqueAgua)    
-    botonViento.addEventListener("click", ataqueViento)     
+    botones = document.querySelectorAll(".BAtaque") 
 }
 function secuenciaAtaques() {
     botones.forEach((boton) =>{
         boton.addEventListener("click", (e) => { 
             if (e.target.textContent == "Fuego🔥") {
-                ataquesJugador.push("Fuego🔥")
+                ataquesDelJugador.push("FUEGO")
+                console.log(ataquesDelJugador)
                 boton.style.background = '#112f58' 
+                boton.disabled = true
             } else if (e.target.textContent == "Agua💧"){
-                ataquesJugador.push("Agua💧")
+                ataquesDelJugador.push("AGUA")
+                console.log(ataquesDelJugador)
                 boton.style.background = '#112f58'
+                boton.disabled = true
             } else {
-                ataquesJugador.push("Viento🌪️")
+                ataquesDelJugador.push("VIENTO")
+                console.log(ataquesDelJugador)
                 boton.style.background = '#112f58'
+                boton.disabled = true
             }
+            ataqueRival()
         })
-    })
-}
-function ataqueFuego() {
-    ataqueJugador = "FUEGO"
-    ataqueRival()
-}
-function ataqueAgua() {
-    ataqueJugador = "AGUA"
-    ataqueRival()
-}
-function ataqueViento() {
-    ataqueJugador = "VIENTO"
-    ataqueRival()
+    })  
 }
 function ataqueRival() {
-    pc = ranNumero(1,3)
-    if (pc == 1) {
-        ataqueDelRival = "FUEGO"
-    } else if (pc == 2) {
-        ataqueDelRival = "AGUA"
+    aleatorio = ranNumero(0,ataquesMokeponEnemigo.length-1)
+    tipoDeAtaqueEnemigo = ataquesMokeponEnemigo[aleatorio].nombre
+    if (tipoDeAtaqueEnemigo == "Fuego🔥") {
+        ataquesDelRival.push("FUEGO")
+    } else if (tipoDeAtaqueEnemigo === "Agua💧") {
+        ataquesDelRival.push("AGUA")
     } else {
-        ataqueDelRival = "VIENTO"
+        ataquesDelRival.push("VIENTO")
     } 
-   addElemento()    
+    console.log(ataquesDelRival)
+    iniciarCombate()    
 }
-function addElemento() {
-    resultado = textBatalla(ataqueJugador, ataqueDelRival)   
-    let newAtaqueDelJugador = document.createElement("p")    
-    let newAtaqueDelPc = document.createElement("p")
-    resultadoBatalla.innerHTML = resultado
-    newAtaqueDelJugador.innerHTML = "Atacaste con "+ ataqueJugador
-    newAtaqueDelPc.innerHTML = "Tu oponente ataco con "+ ataqueDelRival
-    ataqueDelJugador.appendChild(newAtaqueDelJugador)
-    ataqueDelPc.appendChild(newAtaqueDelPc)
-    updateVida()
-    revisarVida()
-}
-function textBatalla(atak1, atak2){
-    if (atak1 == atak2){empates ++; return(" EMPATARON")}
-    else if (atak1 == "FUEGO" && atak2 == "VIENTO"){triunfos++; return(" VICTORIA para el JUGADOR")}
-    else if (atak1 == "AGUA" && atak2 == "FUEGO"){triunfos++; return(" VICTORIA para el JUGADOR")}
-    else if (atak1 == "VIENTO" && atak2 == "AGUA"){triunfos++; return(" VICTORIA para el JUGADOR")}
-    else {derrotas ++; return(" VICTORIA para la MAQUINA")}
-}
-function updateVida() {
-    miVida.innerHTML = vidaTuya - derrotas
-    suVida.innerHTML = vidaMaquina - triunfos
-}
-function revisarVida() {
-    if (triunfos > 3) {
-        alert("GANAMOS")
-        reinicio()
-    } else if (derrotas > 3){
-        alert("PERDIMOS")
-        reinicio()
+function iniciarCombate() {
+    if (ataquesDelJugador.length === 5) {
+        textBatalla()
     }
+}
+function textBatalla(){
+    for (let i = 0; i < ataquesDelJugador.length; i++) {
+        if (ataquesDelJugador[i] === ataquesDelRival[i]) {
+            empates++
+            indexAmbos(i, i)
+            addElemento(triunfos, derrotas, false)
+        } else if (ataquesDelJugador[i] === "FUEGO" && ataquesDelRival[i] === "VIENTO"){
+            triunfos++
+            indexAmbos(i, i)
+            addElemento(triunfos, derrotas, false)
+        } else if (ataquesDelJugador[i] === "AGUA" && ataquesDelRival[i] === "FUEGO"){
+            triunfos++ 
+            indexAmbos(i, i)
+            addElemento(triunfos, derrotas, false)
+        } else if (ataquesDelJugador[i] === "VIENTO" && ataquesDelRival[i] === "AGUA"){
+            triunfos++           
+            indexAmbos(i, i)
+            addElemento(triunfos, derrotas, false)
+        } else {
+            derrotas++
+            indexAmbos(i, i)
+            addElemento(triunfos, derrotas, false)
+        } updateVida(triunfos, derrotas)
+    }resultadoBatalla.innerHTML = addElemento(triunfos, derrotas, true)
+}
+function indexAmbos(jugador, enemigo) {
+    indexAtaqueJugador = ataquesDelJugador[jugador]
+    indexAtaqueEnemigo = ataquesDelRival[enemigo]
+}
+function addElemento(triunfos, derrotas, bool) { 
+    if (bool == false) {
+        let newAtaqueDelJugador = document.createElement("p")    
+    let newAtaqueDelRival = document.createElement("p")
+    newAtaqueDelJugador.innerHTML = indexAtaqueJugador 
+    newAtaqueDelRival.innerHTML = indexAtaqueEnemigo
+    ataqueDelJugador.appendChild(newAtaqueDelJugador)
+    ataqueDelPc.appendChild(newAtaqueDelRival)
+    } else if (triunfos == derrotas) {
+        return resultado = ("EMPATE")
+    } else if (triunfos > derrotas){
+        return resultado = ("VICTORIA")
+    } else {
+        return resultado = ("DERROTA")
+    }   
+}
+function updateVida(triunfos, derrotas) {
+    miVida.innerHTML = triunfos 
+    suVida.innerHTML = derrotas
 }
 function reinicio() {
     location.reload()
